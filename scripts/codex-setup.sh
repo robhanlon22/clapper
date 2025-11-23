7074a2d45bcb087168392cb752b423125bd933b6
Rob Hanlon <69870+robhanlon22@users.noreply.github.com>
2025-11-23 13:24:05 -0800

    Add another dep

--- scripts/codex-setup.sh
+++ scripts/codex-setup.sh
@@ -5,6 +5,6 @@ set -eufo pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 apt-get update
-apt-get install -y libportaudio2
+apt-get install -y portaudio19-dev libportaudio2
 
 scripts/codex-maintenance.sh
